719e9a531b9f886a50bff579f7f1dd9569bdc43e
lntl <l_natale@hotmail.com>
2019-01-15 21:42:27 +0100

    WIP

--- script/main.js
+++ script/main.js
@@ -40,64 +40,89 @@ function startGame(){
   jump();
 }
 
-
+var ctimg=1;
 
 function moveHero(){
-  // console.log(perso)
-  if(perso.action==="play"){
-    if(perso.move_to==="left") {
-      $('#hero').html('<img src=sprite/heros/hero-lm.gif >');
-    } else if(perso.move_to==="right"){
-      $('#hero').html('<img src=sprite/heros/hero-rm.gif>');
-    } else if(perso.move_to==="top"){
-      $('#hero').html('<img src=sprite/heros/hero-t.png>');
-    } 
-  } else if(perso.action==="stop"){
-    if(perso.move_to==="left") {
-      $('#hero').html('<img src=sprite/heros/hero-l.gif>');
-    } else if(perso.move_to==="right"){
-      $('#hero').html('<img src=sprite/heros/hero-r.gif>');
-    } else {
-      $('#hero').html('<img src=sprite/heros/hero-l.png>');
-    }
-  }
+  //console.log(map[5])
+  //console.log(map);
+  //console.log(Math.round(perso.pos.left / 20) - 37);
+  // console.log(perso);
+  console.log(hit);
 }
 
 function jump(){
-  
-  if(perso.cur_jump!= -50){
-    perso.cur_jump++;
-    perso.dir_y -= perso.cur_jump;
-  }
-  if ((perso.pos.top - perso.dir_y >= map[Math.round(perso.pos.left / 20)] - 37) && (perso.pos.top - perso.dir_y <= map[Math.round(perso.pos.left / 20)] - 27)) {
-    perso.cur_jump = -50;
-    perso.dir_y = 0;
-    perso.pos.top = map[Math.round(perso.pos.left / 20)] - 34;
-  } else {
-    if ((perso.pos.top - perso.dir_y < map[Math.round(perso.pos.left / 20)] - 37) && (perso.cur_jump == -50)) {
-      perso.cur_jump = 1;
-    }
-    if ((perso.pos.top - perso.dir_y > map[Math.round(perso.pos.left / 20)] - 27) && (perso.cur_jump == -50)) {
-      perso.cur_jump = 1;
-    }
-  }
-  //droite et gauche
-  if (Math.abs(perso.move) == 5) {
-    perso.pos.left += perso.move;
-  }
 
-  if(perso.dir_y<-100){
+  if(perso.pos.top - perso.dir_y<$("#floor").position().top){
+    if(perso.action==="play"){
+      if(perso.move_to==="left" || perso.move_to==="right") {
+        
+        $('#hero').html('<img src=sprite/heros/hero-m-'+ctimg+'.png>');
+        if(ctimg>0 && ctimg<3){
+          ctimg++;
+        } else {
+          ctimg--;
+        }
+      } else if(perso.move_to==="top"){
+        $('#hero').html('<img src=sprite/heros/hero-t.png>');
+      } else if(perso.move_to==="down"){
+        $('#hero').html('<img src=sprite/heros/hero-b.png>');
+      }
+    } else if(perso.action==="stop"){
+      if(perso.move_to==="left") {
+        $('#hero').html('<img src=sprite/heros/hero-l.png>');
+      } else if(perso.move_to==="right"){
+        $('#hero').html('<img src=sprite/heros/hero-r.png>');
+      } else {
+        $('#hero').html('<img src=sprite/heros/hero-l.png>');
+      }
+    }
+
+    //droite et gauche
+    if (Math.abs(perso.move) == 5) {
+      perso.pos.left += perso.move;
+    }
+    if(perso.cur_jump!= -50){
+      perso.cur_jump++;
+      perso.dir_y -= perso.cur_jump;
+    }
+    if ((perso.pos.top - perso.dir_y >= map[Math.round(perso.pos.left / 20)] - 37) && (perso.pos.top - perso.dir_y <= map[Math.round(perso.pos.left / 20)] - 27)) {
+      perso.cur_jump = -50;
+      perso.dir_y = 0;
+      perso.pos.top = map[Math.round(perso.pos.left / 20)] - 34;
+      // console.log('colision')
+      // console.log(perso.pos.top)
+    } else {
+      if ((perso.pos.top - perso.dir_y < map[Math.round(perso.pos.left / 20)] - 37) && (perso.cur_jump == -50)) {
+        perso.cur_jump = 1;
+
+        console.log('no - jump')
+      }
+      if ((perso.pos.top - perso.dir_y > map[Math.round(perso.pos.left / 20)] - 27) && (perso.cur_jump == -50)) {
+        perso.cur_jump = 1;
+        console.log('jump - down')
+      }
+    }
+  
+    
+    //window.location="";
+  } else {
     perso.status = "dead";
     //alert('Mort :) ');
     $('.end').animate({opacity:1, zIndex:100}, 500)
-    //window.location="";
   }
+  
   //console.log(perso.cur_jump);
   $('#hero').css({'left':perso.pos.left,'top': perso.pos.top - perso.dir_y});
   $('#game').scrollLeft(perso.pos.left - 200, 0);
+  var herotop=perso.pos.top - perso.dir_y;
+  console.log(herotop, hit[0].top)
+  if(herotop<hit[0].top && perso.pos.left>=hit[0].left){
+    console.log('ok')
+  }
+
   //window.scrollTo(perso.pos.left - 200, 0);
   //
-  $('#score span').html(perso.pos.left);
+  
   setTimeout("jump()", 30);
 }
 
